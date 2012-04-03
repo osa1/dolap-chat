2de52fe2303f14ac97bd301a1b152376ae30a706
osa1 <omeragacan@gmail.com>
2012-04-04 02:20:31 +0300

    remove text packs

--- Msg.hs
+++ Msg.hs
@@ -13,10 +13,17 @@ loginFirst = "You should login first."
 unknownCommand :: String -> T.Text
 unknownCommand = T.pack . (++) "Unknown command: "
 
+unknownChan :: T.Text -> T.Text
+unknownChan = mappend "Unknown chan: "
+
+msgCmd :: T.Text -> T.Text -> T.Text -> T.Text
+msgCmd client chan msg =
+    "msg " `mappend` (T.intercalate " " [client, chan, msg])
+
 leftChannelCmd :: T.Text -> T.Text -> T.Text
 leftChannelCmd nick channel =
-    (T.pack "leave ") `mappend` nick `mappend` (T.pack " ") `mappend` channel
+    "leave " `mappend` nick `mappend` " " `mappend` channel
 
 joinChannelCmd :: T.Text -> T.Text -> T.Text
 joinChannelCmd nick channel =
-    (T.pack "join ") `mappend` nick `mappend` (T.pack " ") `mappend` channel
+    "join " `mappend` nick `mappend` " " `mappend` channel
